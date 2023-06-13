678cbb0f1a246b487dc7b69f8cdd1f2b430e8a0a
masyana <zhironkina2003@gmail.com>
2023-06-13 22:00:25 +0300

    init

---
+++ src/layout/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Movies } from "../componets/Movies";
+import { Preloader } from "../componets/preloader";
+import { Search } from "../componets/search";
+
+class Main extends React.Component{
+    state = {movies: [], loading: true,};
+
+    componentDidMount(){
+        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=2b626fc7')
+            .then((response)=> response.json())
+            .then((data)=> this.setState({movies: data.Search, loading: false}));
+    }
+
+    searchMovies = (str, type = "all") => {
+        fetch(`http://www.omdbapi.com/?apikey=108ac68c&s=${str}${type !== "all" ? `&type=${type}` : ""}`)
+        .then((response)=> response.json())
+        .then((data)=> this.setState({movies: data.Search, loading: false}));
+    }
+
+    render() {
+        const { movies, loading } = this.state;
+
+        return (
+            <main className="container content">
+                <Search searchMovies={this.searchMovies} />
+                {loading ? <Preloader /> : <Movies movies={this.state.movies} />}
+            </main>
+        );
+    }
+}
+    
+export{Main};
